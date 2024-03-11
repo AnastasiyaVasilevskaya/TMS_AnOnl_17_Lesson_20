953ad7933dc849aaad236b64de6713a103d08c89
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-03-11 17:55:34 +0300

    change to binding

--- app/src/main/java/com/example/domashka_19/MainActivity.kt
+++ app/src/main/java/com/example/domashka_19/MainActivity.kt
@@ -8,16 +8,20 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.domashka_19.databinding.ActivityMainBinding
+import com.example.domashka_19.databinding.AddNoteDialogBinding
 import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var items: MutableList<ListItem>
     private lateinit var noteAdapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.notes)
+        val recyclerView = binding.notes
         items = mutableListOf(
             Group("Покупки"),
             Note("Евроопт", "Купить 5 кг бананов", LocalDate.parse("2024-03-05")),
@@ -29,23 +33,21 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
 
-        findViewById<AppCompatButton>(R.id.add_note_button).setOnClickListener {
+        binding.addNoteButton.setOnClickListener {
             showAddNoteDialog()
         }
     }
 
 
     private fun showAddNoteDialog() {
+        val dialogBinding = AddNoteDialogBinding.inflate(layoutInflater)
         val builder = AlertDialog.Builder(this)
-        val dialogView = LayoutInflater.from(this).inflate(R.layout.add_note_dialog, null)
-        val titleEditText = dialogView.findViewById<EditText>(R.id.dialog_title)
-        val textEditText = dialogView.findViewById<EditText>(R.id.dialog_text)
 
         builder.setTitle("Добавить заметку")
-            .setView(dialogView)
+            .setView(dialogBinding.root)
             .setPositiveButton("Добавить") { _, _ ->
-                val title = titleEditText.text.toString()
-                val text = textEditText.text.toString()
+                val title = dialogBinding.dialogTitle.text.toString()
+                val text = dialogBinding.dialogText.text.toString()
 
                 val newItem = if (title.isNotEmpty()) {
                     if (text.isNotEmpty()) {
