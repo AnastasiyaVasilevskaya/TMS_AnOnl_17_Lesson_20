947d99e824ca660a37c340116603155aa32ef922
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2024-03-12 21:19:47 +0300

    update

--- app/src/main/java/com/example/domashka_19/GroupViewHolder.kt
+++ app/src/main/java/com/example/domashka_19/GroupViewHolder.kt
@@ -3,19 +3,19 @@ package com.example.domashka_19
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.domashka_19.databinding.NoteItemBinding
+import com.example.domashka_19.databinding.NoteGroupBinding
 
-class GroupViewHolder(private val noteGroupBinding: NoteItemBinding) :
+class GroupViewHolder(private val noteGroupBinding: NoteGroupBinding) :
     RecyclerView.ViewHolder(noteGroupBinding.root) {
 
     fun bind(group: Group) {
-        noteGroupBinding.noteTitle.text = group.groupTitle
+        noteGroupBinding.groupTitle.text = group.groupTitle
     }
 
     companion object {
         fun from(parent: ViewGroup): GroupViewHolder {
             val layoutInflater = LayoutInflater.from(parent.context)
-            val binding = NoteItemBinding.inflate(layoutInflater, parent, false)
+            val binding = NoteGroupBinding.inflate(layoutInflater, parent, false)
             return GroupViewHolder(binding)
         }
     }
